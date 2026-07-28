2a060b373a6eed1a04fb5e32811c862c56d0d91e
vxnxth23 <vineethkumarkumar125@gmail.com>
2026-07-28 11:15:42 +0530

    this is new print

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("welcom to python")
 print("hloo")
 print("vineeth")
+
+print("this is my new python program")
+a=input("enter a number")
+b=input("enter another number")
+sum=int(a)+int(b)
+print("the sum of two numbers is",sum)
